573f6661ff1abf6e9a68299894c7c72e5e4144c8
RC-Yang <some999816@gmail.com>
2025-10-08 23:48:33 +0800

    刪除rest-bean.xml、web.xml，以完全符合Spring Boot設定。

--- RestBookingProject/src/main/java/com/test/TestConfig.java
+++ RestBookingProject/src/main/java/com/test/TestConfig.java
@@ -5,18 +5,30 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class TestConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
 	}
 
 	//這包含Spring JDBC、Spring Data JPA、JPA的交易管理
